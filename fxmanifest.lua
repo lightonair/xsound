87be27f4f501dd5abded9fb208c09ac8e6559642
MrXogos <49158267+Xogy@users.noreply.github.com>
2025-12-03 13:00:50 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,7 @@ rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aw
 
 --[[ Resource Information ]]--
 name         'xsound'
-version      '1.5.1'
+version      '1.0.0-experimental'
 
 client_scripts {
 	"config.lua",
